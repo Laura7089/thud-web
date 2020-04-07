2de6f8799540d193823f4ce0307928201f8c6240
Laura Demkowicz-Duffy <edward70891@gmail.com>
2020-04-07 19:20:48 +0100

    Add error handling to saves

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod game_move;
-mod disk_games;
+mod saves;
 use crate::game_move::Move;
 
 use rocket::request::Form;
@@ -17,30 +18,31 @@ enum ReqResult {
     Board(thud::Board),
     GameOver(thud::EndState, thud::Board),
     SessionExists,
-    Ok,
-    NoSuchSession,
+    Success,
+    Unknown,
+    SessionNotFound,
     BadCoordinate,
     IllegalMove(thud::ThudError),
 }
 
 #[get("/board?<sessionid>")]
 fn game_state(sessionid: SessionID) -> Json<ReqResult> {
-    Json(match disk_games::get_game(sessionid) {
+    Json(match saves::get_game(sessionid) {
         Ok(mut game) => {
             if let Some(state) = game.winner() {
-                disk_games::write_game(sessionid, &game);
+                saves::write_game(sessionid, &game);
                 ReqResult::GameOver(state, game.board())
             } else {
                 ReqResult::Board(game.board())
             }
-        },
+        }
         Err(_) => ReqResult::NoSuchSession,
     })
 }
 
 #[post("/move?<sessionid>", data = "<wanted_move>")]
-fn move_piece(sessionid: SessionID, wanted_move: Form<Move> ) -> Json<ReqResult> {
-    let mut game = match disk_games::get_game(sessionid) {
+fn move_piece(sessionid: SessionID, wanted_move: Form<Move>) -> Json<ReqResult> {
+    let mut game = match saves::get_game(sessionid) {
         Ok(g) => g,
         Err(_) => return Json(ReqResult::NoSuchSession),
     };
@@ -52,16 +54,16 @@ fn move_piece(sessionid: SessionID, wanted_move: Form<Move> ) -> Json<ReqResult>
 
     Json(match game.move_piece(src, dest) {
         Ok(_) => {
-            disk_games::write_game(sessionid, &game);
+            saves::write_game(sessionid, &game);
             ReqResult::Ok
-        },
+        }
         Err(e) => ReqResult::IllegalMove(e),
     })
 }
 
 #[post("/new?<sessionid>")]
 fn new_game(sessionid: SessionID) -> Json<ReqResult> {
-    Json(if disk_games::new_game(sessionid) {
+    Json(if saves::new_game(sessionid) {
         ReqResult::Ok
     } else {
         ReqResult::SessionExists
@@ -70,6 +72,8 @@ fn new_game(sessionid: SessionID) -> Json<ReqResult> {
 
 fn main() {
     let thud = Thud::new();
-    disk_games::write_game(100, &thud);
-    rocket::ignite().mount("/thud", routes![game_state, move_piece, new_game]).launch();
+    saves::write_game(100, &thud);
+    rocket::ignite()
+        .mount("/thud", routes![game_state, move_piece, new_game])
+        .launch();
 }
